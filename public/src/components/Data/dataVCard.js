bbb716ce3032c8683663654d976877b4814ea630
Pipelenisf <98329921+Pipelenisf@users.noreply.github.com>
2023-03-10 16:28:58 -0500

    docs: added data objects

--- public/src/components/Data/dataVCard.js
+++ public/src/components/Data/dataVCard.js
@@ -26,21 +26,84 @@ const dataVideoCard = [
     },
     
     {
-        thumbnail: "",
-        title: "",
-        views: "",
-        channel: "",
-        user: "",
-        date: "",
+        thumbnail: "https://i.ytimg.com/vi/xrZV1058b6M/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBv7n-7ayiXbS2pmA4GRt7amXYCww",
+        title: "Juliana - II. Delirante feat Nico Legreti (Video Oficial)",
+        views: "865,308 vistas",
+        channel: "https://yt3.ggpht.com/QXifKkn_V3cUED9nc21oRqmkZHsq0evSiR2M4hS8909xI8-4K458BZZHhcP035KRWZl-g4whWA=s88-c-k-c0x00ffffff-no-rj",
+        user: "Juliana",
+        date: "hace 1 año",
     },
     
     {
-        thumbnail: "",
-        title: "",
-        views: "",
-        channel: "",
-        user: "",
-        date: "",
+        thumbnail: "https://i.ytimg.com/vi/jD_2e-gziI4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCwPpdrNEA2kd4ktd_XLPq5zqrLew",
+        title: "Ice Spice - Bikini Bottom (Live) | Vevo DSCVR Artist to Watch 2023",
+        views: "575,148 vistas",
+        channel: "https://yt3.ggpht.com/vk3B8pYz3Ng9wST6WGdjLiy9lkfFUkvb3QkE2k8Te8_23JGn05BSVFt_bpFImkITeTSLRdLb7Q=s88-c-k-c0x00ffffff-no-rj",
+        user: "Ice Spice",
+        date: "hace 2 meses",
+    },
+
+    {
+        thumbnail: "https://i.ytimg.com/vi/dMGh801b0Mw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCAHj88DEvHYNnFv1HqBF6oh_fSkQ",
+        title: "Bad Gyal - Sin Carné (Video Oficial)",
+        views: "2.6 M de vistas",
+        channel: "https://yt3.ggpht.com/JLO7ByYgg5L2qLXxS2XXrZc2CHcSVWMbKid4EmAfuyvHCymhI8g0aEnKHZdEm1N0G_8BaB9YFg=s88-c-k-c0x00ffffff-no-rj",
+        user: "Bad Gyal",
+        date: "hace 5 meses",
+    },
+
+    {
+        thumbnail: "https://i.ytimg.com/vi/0k_7oFpbJZQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLABZ5Hm-LnHrUDbV3sdjiOJfKjvyQ",
+        title: "FLO - Summertime",
+        views: "943,565 vistas",
+        channel: "https://yt3.ggpht.com/jMsUVVfLlHCU6yZ7BhzXdK5dq_6S3RCESK4RUaTqklEHF10xiis3rsFKsApGdRmYxhrtXpBZEw=s88-c-k-c0x00ffffff-no-rj",
+        user: "FLO",
+        date: "hace 6 meses",
+    },
+
+    {
+        thumbnail: "https://i.ytimg.com/vi/HjBo--1n8lI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBP45SDTPtoHqGDuNA5eHjANQsbeQ",
+        title: "Rihannas FULL Apple Music Super Bowl LVII Halftime Show",
+        views: "102 M de vistas",
+        channel: "https://yt3.ggpht.com/ytc/AL5GRJUnTGPkOIu3nYWXPfqrqMNQZlsLgh9ms2mlh8IE8YU=s68-c-k-c0x00ffffff-no-rj",
+        user: "NFL",
+        date: "hace 3 semanas",
+    },
+
+    {
+        thumbnail: "https://i.ytimg.com/vi/Y3XacUKTdHc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD_gHbLSrjO-ntMIX1ZP8vX1ppdQg",
+        title: "Bad Gyal - Flow 2000 (Oficial Video)",
+        views: "13 M de vistas",
+        channel: "https://yt3.ggpht.com/JLO7ByYgg5L2qLXxS2XXrZc2CHcSVWMbKid4EmAfuyvHCymhI8g0aEnKHZdEm1N0G_8BaB9YFg=s88-c-k-c0x00ffffff-no-rj",
+        user: "Bad Gyal",
+        date: "hace 1 año",
+    },
+   
+    {
+        thumbnail: "https://i.ytimg.com/vi/mDI4QMeEp0A/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDzHh2fkXrWWCHy-LIouDoxfYPAxw",
+        title: "Snow Tha Product",
+        views: "vistas",
+        channel: "1.7 M de vistas",
+        user: "SNOWTHATPRODUCT",
+        date: "hace 3 meses",
+    },
+   
+    {
+        thumbnail: "https://i.ytimg.com/vi/7T2RonyJ_Ts/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAetNX8k3z5qlEwacCVeZPj6aG63g",
+        title: "Miley Cyrus - Mothers Daughter (Official Video)",
+        views: "134 M de vistas",
+        channel: "https://yt3.ggpht.com/c_zqSYR5xP7lgDO8xZtbQ67MXhOel1cHlSpUTvNgQN-QOEfLcLan6-owiGECb5xE8Qid3zJUxA=s88-c-k-c0x00ffffff-no-rj",
+        user: "Miley Cyrus",
+        date: "hace 3 años",
+    },
+
+    {
+        thumbnail: "https://i.ytimg.com/vi/2p3zZoraK9g/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBYzRDcmUOypUE6VuF8rX4NB8sUxA",
+        title: "SZA - Good Days (Official Video)",
+        views: "81 M de vistas",
+        channel: "https://yt3.ggpht.com/_W2XMP5_R2HP_WXN958k-K2bHvfm69ixeP_2Tfrfg9QxuPOREoMztZ8QGIwhs_7QkhuDoi1K-ho=s88-c-k-c0x00ffffff-no-rj",
+        user: "SZA",
+        date: "hace 2 años",
     },
 
 ]
